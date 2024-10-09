76dcc8a714271c3f2796cfe4ab6d2ce1983f2ef3
mati22124 <mayanktiku04@gmail.com>
2024-10-08 23:48:29 -0400

    Initial Commit

---
+++ hurricaneService/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hurricaneService
+//
+//  Created by Mayank Tiku on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hurricaneService/hurricaneServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  hurricaneServiceApp.swift
+//  hurricaneService
+//
+//  Created by Mayank Tiku on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct hurricaneServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
